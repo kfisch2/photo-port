61f9f249f25e703583b15d324333696c25cb68c1
kfisch2 <kmlindsey24@gmail.com>
2022-09-11 18:50:05 -0700

    Nav test

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,28 +1,27 @@
 import React from "react";
 
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup } from "@testing-library/react";
 
-import '@testing-library/jest-dom/extend-expect';
+import "@testing-library/jest-dom/extend-expect";
 
 import About from "..";
 
-// after each test, we won't have leftover memory data 
+// after each test, we won't have leftover memory data
 afterEach(cleanup);
 
-describe('About component', () => {
+describe("About component", () => {
   // renders About test
   // First test
   // 'it' first arg = string of what's being tested
   //      second arg =  cb that runs the test
-  it('renders', () => {
-    render(<About />)
-  })
+  it("renders", () => {
+    render(<About />);
+  });
 
   // Second test
-  it('matches snapshot DOM node structure', () => {
+  it("matches snapshot DOM node structure", () => {
     // render About
     const { asFragment } = render(<About />);
     expect(asFragment()).toMatchSnapshot();
-  })
-})
-
+  });
+});
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -26,10 +26,10 @@ function Nav() {
   }
 
   return (
-    <header>
+    <header className="flex-row px-1">
       <h2>
-        <a href="/">
-          <span role="img" arial-label="camera">
+        <a data-testid="link" href="/">
+          <span role="img" aria-label="camera">
             📸
           </span>
           Oh Snap!
@@ -38,7 +38,7 @@ function Nav() {
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a href="#about">About me</a>
+            <a data-testid="about" href="#about">About me</a>
           </li>
           <li>
             <span>Contact</span>
